a2aa19fae8eadf6edc82282df0062a622b130e02
Geoffrey Goilot <geoffreygoilot@gmail.com>
2021-01-18 12:14:59 +0100

    first commit: create contactCard

---
+++ src/Components/Contact.js
@@ -0,0 +1,21 @@
+import './Contact.css';
+const isOnline = true;
+
+function Contact () {
+    return (
+        <div className="Contact">
+            <div>
+                <img className='avatar' src='https://i.postimg.cc/BQW4FPd6/pikachu.png'></img>
+            </div>
+            <div>
+                <h4 className="name"> Geoff Gual </h4>
+                <div className="status">
+                    <div className={"status-" + (isOnline ? 'online': 'offline')}></div>
+                    <p className="status-text"> {isOnline ? 'Online': 'Offline'}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
